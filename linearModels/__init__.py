5a318ffc3c0027b1eed4209ae5dfda2fcd6e0ac8
mortezai <morteza2006g@gmail.com>
2025-09-18 18:48:51 +0330

    feat(linearModels): implement ridge regression and update init file

--- linearModels/__init__.py
+++ linearModels/__init__.py
@@ -1,6 +1,7 @@
 from .linearRegression import LinearRegression
 from .logisticRegression import LogisticRegression
 from .lassoRegression import LassoRegression
+from .ridgeRegression import RidgeRegression
 
 
-__all__ = ["LinearRegression", "LogisticRegression", "LassoRegression"]
+__all__ = ["LinearRegression", "LogisticRegression", "LassoRegression", "RidgeRegression"]
