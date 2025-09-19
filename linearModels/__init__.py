08e4739debd0975b4182534c5fb0d7e248dfb2e6
mortezai <morteza2006g@gmail.com>
2025-09-19 11:19:49 +0330

    feat(linearModels): implement softmax regression and update init file

--- linearModels/__init__.py
+++ linearModels/__init__.py
@@ -2,6 +2,8 @@ from .linearRegression import LinearRegression
 from .logisticRegression import LogisticRegression
 from .lassoRegression import LassoRegression
 from .ridgeRegression import RidgeRegression
+from .softmaxRegression import SoftmaxRegression
 
 
-__all__ = ["LinearRegression", "LogisticRegression", "LassoRegression", "RidgeRegression"]
+__all__ = ["LinearRegression", "LogisticRegression", "LassoRegression"
+           , "RidgeRegression", "SoftmaxRegression"]
