967136c5aedd2c376ccd594460719509441c31df
mortezai <morteza2006g@gmail.com>
2025-09-19 20:34:00 +0330

    feat(linearModels): implement elastic net and update init file

--- linearModels/__init__.py
+++ linearModels/__init__.py
@@ -4,7 +4,9 @@ from .lassoRegression import LassoRegression
 from .ridgeRegression import RidgeRegression
 from .softmaxRegression import SoftmaxRegression
 from .poissonRegression import PoissonRegression
+from .elasticNet import ElasticNet
 
 
 __all__ = ["LinearRegression", "LogisticRegression", "LassoRegression"
-           , "RidgeRegression", "SoftmaxRegression", "PoissonRegression"]
+           , "RidgeRegression", "SoftmaxRegression", "PoissonRegression"
+           , "ElasticNet"]
