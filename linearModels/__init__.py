b4fbe49c7934fff6b811ac1d5e7a6644402f6899
mortezai <morteza2006g@gmail.com>
2025-09-19 11:24:15 +0330

    feat(linearModels): implement poisson regression and update init file

--- linearModels/__init__.py
+++ linearModels/__init__.py
@@ -3,7 +3,8 @@ from .logisticRegression import LogisticRegression
 from .lassoRegression import LassoRegression
 from .ridgeRegression import RidgeRegression
 from .softmaxRegression import SoftmaxRegression
+from .poissonRegression import PoissonRegression
 
 
 __all__ = ["LinearRegression", "LogisticRegression", "LassoRegression"
-           , "RidgeRegression", "SoftmaxRegression"]
+           , "RidgeRegression", "SoftmaxRegression", "PoissonRegression"]
